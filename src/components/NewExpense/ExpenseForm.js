b35e0b7ba35bad13e2d03fff10a5c1a2e7d4ee29
code-gram <mail.codegram@gmail.com>
2023-02-03 00:25:12 +0530

    Single State demo on expense form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,34 +1,50 @@
 import React, { useState } from "react";
 import "./ExpenseForm.css";
 const ExpenseForm = () => {
-  const [enteredTitle, setEnteredTitle] = useState("");
-  const [enteredAmount, setEnteredAmount] = useState("");
-  const [enteredDate, setEnteredDate] = useState("");
+  const [userInput, setUserInput] = useState({
+    enteredTitle: "",
+    enteredAmount: "",
+    enteredDate: "",
+  });
   const titleChangeHandler = (event) => {
     //console.log("titleChangeHandler triggered");
     // console.log(event.target.value);
-    setEnteredTitle(event.target.value);
+    // setEnteredTitle(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value,
+    });
   };
   const amountChangeHandler = (event) => {
     //console.log(event.target.value);
-    setEnteredAmount(event.target.value);
+    // setEnteredAmount(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredAmount: event.target.value,
+    });
   };
   const dateChangeHandler = (event) => {
     // console.log(event.target.value);
-    setEnteredDate(event.target.value);
+    // setEnteredDate(event.target.value);
+    setUserInput({
+      ...userInput,
+      enteredDate: event.target.value,
+    });
   };
 
   const submitHandler = (event) => {
     event.preventDefault();
     const newExpense = {
-      title: enteredTitle,
-      amount: enteredAmount,
-      date: enteredDate,
+      title: userInput.enteredTitle,
+      amount: userInput.enteredAmount,
+      date: userInput.enteredDate,
     };
     console.log(newExpense);
-    setEnteredTitle("");
-    setEnteredAmount("");
-    setEnteredDate("");
+    setUserInput({
+      enteredTitle: "",
+      enteredAmount: "",
+      enteredDate: "",
+    });
   };
   return (
     <form onSubmit={submitHandler}>
@@ -38,7 +54,7 @@ const ExpenseForm = () => {
           <input
             type="text"
             onChange={titleChangeHandler}
-            value={enteredTitle}
+            value={userInput.enteredTitle}
           />
         </div>
         <div className="new-expense__control">
@@ -46,7 +62,7 @@ const ExpenseForm = () => {
           <input
             type="number"
             onChange={amountChangeHandler}
-            value={enteredAmount}
+            value={userInput.enteredAmount}
           />
         </div>
         <div className="new-expense__control">
@@ -56,7 +72,7 @@ const ExpenseForm = () => {
             min="2019-01-01"
             max="2022-12-31"
             onChange={dateChangeHandler}
-            value={enteredDate}
+            value={userInput.enteredDate}
           />
         </div>
         <div className="new-expense__actions">
