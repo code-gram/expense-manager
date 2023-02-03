d5ae1e7a250e3d0c3ccd0c5abc50dbe58f278d37
code-gram <mail.codegram@gmail.com>
2023-02-03 07:09:05 +0530

    Expense Filter Added in Expenses

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
   //this list of expenses will be coming from back end.
   const expenses = [
     { id: 1, title: "Shoping", date: new Date(2023, 1, 1), amount: 5000 },
-    { id: 2, title: "Car washing", date: new Date(2023, 1, 5), amount: 500 },
-    { id: 3, title: "School fee", date: new Date(2023, 1, 5), amount: 50000 },
-    { id: 4, title: "dinner", date: new Date(2023, 1, 5), amount: 5000 },
+    { id: 2, title: "Car washing", date: new Date(2021, 4, 5), amount: 500 },
+    { id: 3, title: "School fee", date: new Date(2020, 3, 5), amount: 50000 },
+    { id: 4, title: "dinner", date: new Date(2019, 2, 5), amount: 5000 },
   ];
   const addExpenseHandler = (expense) => {
     console.log(expense);
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,24 @@
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import ExpensesFilter from "./ExpenseFilter";
+import { useState } from "react";
 
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
   const { expenses } = props;
+  const filterChangeHandler = (selectedYear) => {
+    // console.log("Expenses.js");
+    // console.log(selectedYear);
+    setFilteredYear(selectedYear);
+  };
+
   return (
     <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
       {expenses.map((expense) => {
         return <ExpenseItem key={expense.id} expense={expense} />;
       })}
